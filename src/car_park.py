c56efb13482dec5657e50ad5e8bba24ed2f479df
J155917 <J155917@tafe.wa.edu.au>
2024-11-07 18:27:50 +0800

    feat: Implement initial carpark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,4 +1,17 @@
+from display import Display
+from sensor import Sensor
+
+
 class CarPark:
-    def __init__(self,):
-        pass
+    def __init__(self, locations, displays: [Display]=None, sensors: [Sensor]=None):
+        self.location = locations
+        self.display = displays or []
+        self.sensor = sensors or []
+        self.available_car_bays = 150
+        self.car_plates = [str]
+
+    def __str__(self):
+        return f"Location: {self.location}\n Available Car Bays: {self.available_car_bays}"
+
+
 
